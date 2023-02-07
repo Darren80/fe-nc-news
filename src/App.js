2f5b10a2af2b58edb4b8c6b113a1e4b31da1fdbd
Darren Ohonba - Evans <we444465@gmail.com>
2023-02-07 14:38:45 +0000

    cards

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import ArticleCard from './components/ArticleCard';
+import ArticleCards from './components/ArticleCards';
 
 function App() {
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <ArticleCard/>
+        <ArticleCards />
       </header>
     </div>
   );
--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,12 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 const { getArticles } = require('../endpoints');
 
-function ArticleCard() {
-    getArticles()
-        .then((articles) => console.log(articles))
+function ArticleCard({ article }) {
 
     return (
-        <div>ArticleCard</div>
+        <div>
+            {article ?
+                <div className='article_card' key={article.article_id}>
+                    <h1>{article.title}</h1>
+                    <img src={article.article_img_url} />
+                    <p>{article.body}</p>
+                </div>
+                : null}
+        </div>
     )
 }
 
